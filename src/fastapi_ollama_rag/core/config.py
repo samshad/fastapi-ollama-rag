d1290b360ae761bc92adfecf6ff932bb81224a32
Samshad <samshad.ru@gmail.com>
2026-02-22 03:14:08 -0400

    ci: update github actions trigger branch to master

--- src/fastapi_ollama_rag/core/config.py
+++ src/fastapi_ollama_rag/core/config.py
@@ -6,11 +6,9 @@ class Settings(BaseSettings):
     """
     Core application settings. Fails fast if required variables are missing.
     """
+
     model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        case_sensitive=False,
-        extra="ignore"
+        env_file=".env", env_file_encoding="utf-8", case_sensitive=False, extra="ignore"
     )
 
     project_name: str = "FastAPI Ollama RAG"
@@ -24,6 +22,7 @@ class Settings(BaseSettings):
     ollama_base_url: str = Field(default="http://host.docker.internal:11434")
     ollama_generation_model: str = Field(default="deepseek-r1:8b")
     ollama_embedding_model: str = Field(default="mxbai-embed-large")
+
 
 # Instantiate settings to be imported across the application
 settings = Settings()
